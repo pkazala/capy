d413bd74079b9063e42d0f5e116e7cc7b8b59ce7
Nam Nguyen Hai CSM2020 <nam.nguyen-hai.2020@uni.strath.ac.uk>
2023-03-04 21:17:06 +0000

    function calculate Distance added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,32 @@
 import Script from "./Script";
+import { createEffect } from 'solid-js';
 
 function Map() {
   Script('https://polyfill.io/v3/polyfill.min.js?features=default')
   let map;
+
+  
+  createEffect(()=> {
+    console.log(calculateDistance(55.95333709746188,-3.194140510650442,55.95275837083679,-3.1903639120341842));
+  })
+
+  // distance in km
+  const calculateDistance = (lat1,lon1,lat2,lon2)  => {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }
+  
+  const deg2rad = (deg) =>  {
+    return deg * (Math.PI/180)
+  }
 
   function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
