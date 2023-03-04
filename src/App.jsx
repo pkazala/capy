463a281e26c0229a089b27697e5d2409d9b06420
george-ai-ob <george.ai.obrien@gmail.com>
2023-03-04 19:04:06 +0000

    fixed inputs

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,25 @@ import SearchBar from './components/SearchBar';
 
 function App() {
 
-  const [input, setInput] = createSignal("");
-  const validInputs = ["apple", "banana", "crack"];
+  const [distance, setDistance] = createSignal("");
+  const [diet, setDiet] = createSignal("");
+  const [excludeIngr, setExcludeIngr] = createSignal("");
+  const diets = ["vegan", "vegetarian", "ketogenic", "gluten free", "pescetarian"];
   const [ingredients, setIngredients] = createSignal([]);
 
   createEffect(() => {
-    console.log(input());
-    setInput("");
+    console.log(distance());
+    setDistance("");
+  })
+
+  createEffect(() => {
+    console.log(diet());
+    setDiet("");
+  })
+
+  createEffect(() => {
+    console.log(excludeIngr());
+    setExcludeIngr("");
   })
 
   createEffect(async() => {
@@ -23,11 +35,11 @@ function App() {
     <section class='flex m-auto flex-col items-center'>
       <img src={logo} alt="capy" class='max-w-xl' />
       <label for="distance">Enter travel distance</label>
-      <SearchBar input={input} setInput={setInput} arr={Array.from({ length: 1000 }, (_, i) => i + 1)} id="distance" />
+      <SearchBar input={distance} setInput={setDistance} arr={Array.from({ length: 1000 }, (_, i) => i + 1)} id="distance" />
       <label for="excludeFoods">Enter exluded ingredients</label>
-      <SearchBar input={input} setInput={setInput} arr={ingredients()} id="excludeFoods" />
+      <SearchBar input={excludeIngr} setInput={setExcludeIngr} arr={ingredients()} id="excludeFoods" />
       <label for="dietary">Enter dietary requirements</label>
-      <SearchBar input={input} setInput={setInput} arr={validInputs} id="dietary" />
+      <SearchBar input={diet} setInput={setDiet} arr={diets} id="dietary" />
       <Map/>
       <header class='font-bold text-5xl'>Capy Bank</header>
     </section>
