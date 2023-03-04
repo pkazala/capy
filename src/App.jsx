892555c550a65c5e9852860897b18035eeacd390
Piotrek <pepe.kazala@gmail.com>
2023-03-04 15:15:37 +0000

    Latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import logo from './assets/CAPY_BANK.png';
+import Map from './components/Map.jsx'
 
 function App() {
   return (
     <section class='flex m-auto flex-col items-center'>
       <img src={logo} alt="capy" class='max-w-xl' />
+      <Map/>
       <header class='font-bold text-5xl'>Capy Bank</header>
     </section>
   );
---
+++ src/components/Map.jsx
@@ -0,0 +1,16 @@
+function Map() {
+  return (
+    <iframe
+      width="600"
+      height="450"
+      style="border:0"
+      loading="lazy"
+      allowfullscreen
+      referrerpolicy="no-referrer-when-downgrade"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAI_jxFJ4mzpXRshsvPQw3eRi5KvEFTiT0
+    &q=Space+Needle,Seattle+WA"
+    ></iframe>
+  );
+}
+
+export default Map;
