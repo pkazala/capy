7e7aff82f6dd84214d99e57214f66dd596f5c37c
Piotrek <pepe.kazala@gmail.com>
2023-03-04 19:36:49 +0000

    It works here i guess

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,31 +1,35 @@
 import { createSignal } from "solid-js";
 
 const SearchBar = (props) => {
-    const {id, input, setInput, arr} = props;
+  const { id, input, setInput, arr } = props;
 
-    const [invalid, setInvalid] = createSignal(false)
+  const [invalid, setInvalid] = createSignal(false);
 
-    const updateInput = (event) => {
-        const current = event.target.value;
-        let didSet = false 
-        arr.filter((elem)=>{
-            if (current == elem){
-                setInvalid(false);
-                didSet = true;
-                setInput(current);
-            }
-        })
-        if(!didSet){
-            setInvalid(true);
-        }
-    };
+  const updateInput = (event) => {
+    const current = event.target.value;
+    let didSet = false;
+    arr.filter((elem) => {
+      if (current == elem) {
+        setInvalid(false);
+        didSet = true;
+        setInput(current);
+      }
+    });
+    if (!didSet) {
+      setInvalid(true);
+    }
+  };
 
-    return (
-        <>
-            <label for={id} style={invalid() ? "" : "display:none"}>Error: Please enter valid item</label>
-            <input id={id} type="text" value={input()} onChange={updateInput} />
-        </>
-    );
+  return (
+    <>
+      <div class="m-2">
+        <label for={id} style={invalid() ? "" : "display:none"}>
+          Error: Please enter valid item
+        </label>
+        <input id={id} type="text" value={input()} onChange={updateInput} />
+      </div>
+    </>
+  );
 };
 
 export default SearchBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      main: ["Roboto", "sans-serif"],
+    },
   },
   plugins: [],
 };
