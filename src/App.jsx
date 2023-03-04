efc9bf9cc77bec028f69d4174aefb3889ec8ada9
george.ai.ob <george.ai.obrien@gmail.com>
2023-03-04 16:31:09 +0000

    added search bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { createEffect, createSignal } from 'solid-js';
 import logo from './assets/CAPY_BANK.png';
 import Map from './components/Map.jsx'
+import SearchBar from './components/SearchBar';
 
 function App() {
+
+  const [input, setInput] = createSignal("");
+  const validInputs = ["apple", "banana", "crack"];
+
+  createEffect(() => {
+    console.log(input());
+    setInput("");
+  })
+
   return (
     <section class='flex m-auto flex-col items-center'>
       <img src={logo} alt="capy" class='max-w-xl' />
+      <SearchBar input={input} setInput={setInput} arr={validInputs} />
       <Map/>
       <header class='font-bold text-5xl'>Capy Bank</header>
     </section>
