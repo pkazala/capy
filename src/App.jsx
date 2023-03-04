310aaee88aeb8e5200cda26a8f6e01bffb2b2889
Piotrek <pepe.kazala@gmail.com>
2023-03-04 18:54:13 +0000

    Some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import logo from './assets/CAPY_BANK.png';
 import Map from './components/Map.jsx'
+import Sidebar from './components/Sidebar.jsx'
 
 function App() {
   return (
-    <section class='flex m-auto flex-col items-center'>
-      <img src={logo} alt="capy" class='max-w-xl' />
+    <section class='flex m-auto w-screen h-screen'>
+      <Sidebar/>
       <Map/>
-      <header class='font-bold text-5xl'>Capy Bank</header>
     </section>
   );
 }
