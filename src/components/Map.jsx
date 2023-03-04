1f62b5078781ec58600c0f3833d3b202bf1b6232
Piotrek <pepe.kazala@gmail.com>
2023-03-04 17:54:17 +0000

    Hacky solution to google maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,18 @@
+import Script from "./Script";
+
 function Map() {
-  return (
-    <iframe
-      width="600"
-      height="450"
-      style="border:0"
-      loading="lazy"
-      allowfullscreen
-      referrerpolicy="no-referrer-when-downgrade"
-      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAI_jxFJ4mzpXRshsvPQw3eRi5KvEFTiT0
-    &q=Space+Needle,Seattle+WA"
-    ></iframe>
-  );
+  Script('https://polyfill.io/v3/polyfill.min.js?features=default')
+  let map;
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  }
+  Script('https://maps.googleapis.com/maps/api/js?key=AIzaSyD8OAF_09DCaMjEz6MdVSYqdPc7JreJybQ&callback=initMap&v=weekly')
+  window.initMap = initMap;
+  return <div id="map" class="w-96 h-96"></div>;
 }
 
 export default Map;
---
+++ src/components/Script.jsx
@@ -0,0 +1,18 @@
+import { createEffect } from "solid-js";
+
+function Script(url) {
+  createEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = url;
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  });
+}
+
+export default Script;
