1ad2ef7f8cc5a2785450f043ef29131984392b55
Piotrek <pepe.kazala@gmail.com>
2023-03-04 14:44:18 +0000

    Added components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from './assets/CAPY_BANK.png';
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p class='font-bold'>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <section class='flex m-auto flex-col items-center'>
+      <img src={logo} alt="capy" class='max-w-xl' />
+      <header class='font-bold text-5xl'>Capy Bank</header>
+    </section>
   );
 }
 
