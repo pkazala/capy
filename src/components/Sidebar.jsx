39c037a267e9d00e2c56dba00dad060cd988fe78
Piotrek <pepe.kazala@gmail.com>
2023-03-05 03:25:27 +0000

    css fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ function Sidebar() {
     setIngredients(await response.json());
   });
   return (
-    <section class="w-1/4 max-w-72 flex flex-col items-center border-r-2 h-full min-h-screen shadow-lg text-left text-lg xl:text-2xl">
+    <section class="w-1/4 max-w-72 flex flex-col items-center border-r-2 pb-10 h-max min-h-screen shadow-lg text-left text-lg xl:text-2xl">
       <img src={logo} alt="capy" class="my-4 lg:mx-8" />
       {!isSearched() ? (
       <div class="flex flex-col items-start mx-10">
@@ -55,11 +55,11 @@ function Sidebar() {
         </button>
       </div>
       ) : (
-        <div>
-          <button onClick={() => setIsSearched(false)} class="self-center w-full text-2xl bg-[#00539F] p-1 text-white rounded-xl mt-3 hover:shadow-lg hover:mt-2 transition-all duration-300 hover:bg-blue-500">
+        <div class="flex flex-col gap-4 items-center mx-10 mr-[3.65rem]">
+          <Results></Results>
+          <button onClick={() => setIsSearched(false)} class="self-center w-9/12 text-2xl bg-[#00539F] p-1 text-white rounded-xl mt-3 hover:shadow-lg hover:mt-2 transition-all duration-300 hover:bg-blue-500">
             Go back
           </button>
-          <Results></Results>
         </div>
       )}
     </section>
