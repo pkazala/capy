b7ad5805bcee94528dbd129cbb22e5ce88622800
Piotrek <pepe.kazala@gmail.com>
2023-03-05 04:59:28 +0000

    pls wrk smoehow

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,12 +3,15 @@ import bagel from "../assets/bagel.png";
 import { createEffect, createSignal } from "solid-js";
 import cappy from "../assets/cappy.png";
 
-function Map() {
+function Map(props) {
   Script("https://polyfill.io/v3/polyfill.min.js?features=default");
   let map;
 
+
+  let [loc,setLoc] = props.playerLoc;
+
   let [locationCount, setLocationCount] = createSignal(0);
-  let [loc, setLoc] = createSignal([]);
+
   let [center, setC] = createSignal(0);
   function getLocation() {
     if (navigator.geolocation) {
@@ -30,205 +33,12 @@ function Map() {
   }
   createEffect(() => {
     getLocation();
-    center();
-    loc();
   });
 
-  createEffect(() => {
-    console.log(
-      calculateDistance(
-        55.95333709746188,
-        -3.194140510650442,
-        55.95275837083679,
-        -3.1903639120341842
-      )
-    );
-  });
 
-  // distance in km
-  const calculateDistance = (lat1, lon1, lat2, lon2) => {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2 - lat1); // deg2rad below
-    var dLon = deg2rad(lon2 - lon1);
-    var a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(deg2rad(lat1)) *
-        Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distance in km
-    return d;
-  };
+  const shops=props.shops
 
-  const deg2rad = (deg) => {
-    return deg * (Math.PI / 180);
-  };
-  const shops = [
-    {
-      title: "Sainsbury's",
-      Lat: 55.95385154368854,
-      Lon: -3.194370456886452,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.949063888569064,
-      Lon: -3.189514369792723,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.946435470253384,
-      Lon: -3.1812146030759427,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.94469887960705,
-      Lon: -3.1909595409774085,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.94138349407988,
-      Lon: -3.1830218464726507,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.93863400095199,
-      Lon: -3.1776829585610447,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.946437413813335,
-      Lon: -3.2006310538438365,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.93665631556673,
-      Lon: -3.197673533205771,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.9465988281203,
-      Lon: -3.195795114460836,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.9459927792938,
-      Lon: -3.210086245316522,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.94465897177873,
-      Lon: -3.2110243266867613,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.94203146185667,
-      Lon: -3.2164366231323682,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.957107569334525,
-      Lon: -3.1994025214387443,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Sainsbury's",
-      Lat: 55.95844138217219,
-      Lon: -3.2097270115166827,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Tesco",
-      Lat: 55.94522887871374,
-      Lon: -3.183496555234711,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Tesco",
-      Lat: 55.9486586942626,
-      Lon: -3.185128269690482,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Tesco",
-      Lat: 55.95070338153543,
-      Lon: -3.177226817339035,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Tesco",
-      Lat: 55.936404084488515,
-      Lon: -3.1802697204901813,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Tesco",
-      Lat: 55.94468701939951,
-      Lon: -3.205127861731589,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Tesco",
-      Lat: 55.94366702845466,
-      Lon: -3.210286278155731,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Tesco",
-      Lat: 55.950498532187844,
-      Lon: -3.207678316630654,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Tesco",
-      Lat: 55.95743127388005,
-      Lon: -3.190498710992911,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Tesco",
-      Lat: 55.95783939470905,
-      Lon: -3.1965087912209715,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Lidl",
-      Lat: 55.94578624452127,
-      Lon: -3.184618783062567,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Co-op",
-      Lat: 55.94072359534659,
-      Lon: -3.1780968570848587,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Co-op",
-      Lat: 55.9393302830312,
-      Lon: -3.192241874564685,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-    {
-      title: "Co-op",
-      Lat: 55.938380481505554,
-      Lon: -3.195489267325286,
-      Ing: ["Bread", "vinegar", "Grapes"],
-    },
-  ];
-  let location = false;
+  // let location = false;
 
   function initMap() {
     let UserMarker = "";
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,10 +4,11 @@ import SearchBar from "./SearchBar";
 import Picker from "./Picker";
 import { createEffect, createSignal } from "solid-js";
 
-function Sidebar() {
-  const [distance, setDistance] = createSignal("");
+function Sidebar(props) {
+  const [distance, setDistance] = props.distanceSignal;
   const [diet, setDiet] = createSignal("");
   const [excludeIngr, setExcludeIngr] = createSignal("");
+  const [includeIngr, setIncludeIngr] = createSignal("");
   const diets = [
     "vegan",
     "vegetarian",
@@ -16,42 +17,87 @@ function Sidebar() {
     "pescetarian",
   ];
   const [ingredients, setIngredients] = createSignal([]);
+
+  const [dietArr, setDietArr] = props.dietSignal;
+  const [exIngrArr, setExIngrArr] = props.excludeSignal;
+  const [incIngrArr, setIncIngrArr] = props.includeSignal;
+
   const [isSearched, setIsSearched] = createSignal(false);
+
   createEffect(async () => {
     const response = await fetch("./src/assets/ingredients.json");
     setIngredients(await response.json());
   });
+
+  createEffect(() => {
+    setDietArr((d) => (diet() == "" ? d : [...d, diet()]));
+    setDiet("");
+  });
+
+  createEffect(() => {
+    setExIngrArr((i) => (excludeIngr() == "" ? i : [...i, excludeIngr()]));
+    setExcludeIngr("");
+  });
+
+  createEffect(() => {
+    setIncIngrArr((i) => (includeIngr() == "" ? i : [...i, includeIngr()]));
+    setIncludeIngr("");
+  });
+
+  function exec() {
+    setIsSearched(true);
+    props.search();
+  }
+
   return (
     <section class="w-1/4 max-w-72 flex flex-col items-center border-r-2 pb-10 h-max min-h-screen shadow-lg text-left text-lg xl:text-2xl">
       <img src={logo} alt="capy" class="my-4 lg:mx-8" />
       {!isSearched() ? (
-      <div class="flex flex-col items-start mx-10">
-        <p for="distance">Enter travel distance:</p>
-        <SearchBar
-          input={distance}
-          setInput={setDistance}
-          arr={() => {
-            return Array.from({ length: 1000 }, (_, i) => i + 1);
-          }}
-          id="distance"
-        />
-        <p for="excludeFoods">Enter exluded ingredients:</p>
-        <SearchBar
-          input={excludeIngr}
-          setInput={setExcludeIngr}
-          arr={ingredients}
-          id="excludeFoods"
-        />
-        <p for="dietary">Enter dietary requirements:</p>
-        <Picker diets={diets}/>
-        <button onClick={() => setIsSearched(true)} class="self-center w-full text-2xl bg-[#00539F] p-1 text-white rounded-xl mt-3 hover:shadow-lg hover:mt-2 transition-all duration-300 hover:bg-blue-500">
-          Search
-        </button>
-      </div>
+        <div class="flex flex-col items-start mx-10">
+          <p for="distance">Enter travel distance:</p>
+          <SearchBar
+            input={distance}
+            setInput={setDistance}
+            arr={() => {
+              return Array.from({ length: 1000 }, (_, i) => i + 1);
+            }}
+            id="distance"
+          />
+          <p for="excludeFoods">Enter exluded ingredients:</p>
+          <SearchBar
+            input={excludeIngr}
+            setInput={setExcludeIngr}
+            arr={ingredients}
+            id="excludeFoods"
+          />
+          <p for="includeFood">Enter food in cupboard:</p>
+          <SearchBar
+            input={includeIngr}
+            setInput={setIncludeIngr}
+            arr={() => {
+              return includeIngr;
+            }}
+            id="includeFoods"
+          />
+          <ul>
+            <For each={incIngrArr()}>{(item, i) => <li>{item}</li>}</For>
+          </ul>
+          <p for="dietary">Enter dietary requirements:</p>
+          <Picker diets={diets} />
+          <button
+            onClick={exec}
+            class="self-center w-full text-2xl bg-[#00539F] p-1 text-white rounded-xl mt-3 hover:shadow-lg hover:mt-2 transition-all duration-300 hover:bg-blue-500"
+          >
+            Search
+          </button>
+        </div>
       ) : (
         <div class="flex flex-col gap-4 items-center mx-10 mr-[3.65rem]">
           <Results></Results>
-          <button onClick={() => setIsSearched(false)} class="self-center w-9/12 text-2xl bg-[#00539F] p-1 text-white rounded-xl mt-3 hover:shadow-lg hover:mt-2 transition-all duration-300 hover:bg-blue-500">
+          <button
+            onClick={() => setIsSearched(false)}
+            class="self-center w-9/12 text-2xl bg-[#00539F] p-1 text-white rounded-xl mt-3 hover:shadow-lg hover:mt-2 transition-all duration-300 hover:bg-blue-500"
+          >
             Go back
           </button>
         </div>
